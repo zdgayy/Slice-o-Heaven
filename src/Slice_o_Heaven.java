62a356a7f1331e23f92ea6792258da3e62bb8d80
zdgayy <2110772477@qq.com>
2025-02-25 14:41:47 +0800

    message

--- src/Slice_o_Heaven.java
+++ src/Slice_o_Heaven.java
@@ -1,8 +1,9 @@
 public class Slice_o_Heaven {
-    private String storemenu;
+    public String storemenu;
     public String storeName;
+    public String storeAddress;
     public String storeEmail;
-    public long storePhone;
+    public String storePhone;
     public String pizzaIngredients;
     public double pizzaPrice;
     public String sides;
@@ -10,6 +11,94 @@ public class Slice_o_Heaven {
     private String orderID;
     private double orderTotal;
  
+    private static final String DEF_ORDER_ID = "DEF-SON-099";
+    private static final String DEF_PIZZA_INGREDIENTS = "Mozzarella Cheese";
+    private static final double DEF_ORDER_TOTAL = 15.00;
+   
+   
+    public Slice_o_Heaven() {
+    this.orderID = DEF_ORDER_ID;
+    this.orderTotal = DEF_ORDER_TOTAL;
+    this.pizzaIngredients = DEF_PIZZA_INGREDIENTS;
+    this.sides = "";
+    this.drinks = "";
+    
+    }
+
+    public Slice_o_Heaven(String orderID,String pizzaIngredients,double orderTotal) {
+        this.orderID = orderID;
+        this.pizzaIngredients = pizzaIngredients;
+        this.orderTotal = orderTotal;    
+    }
+
+    public String getStoremenu() {
+        return storemenu;
+    }
+    public String getStoreName() {
+        return storeName;
+    }
+    public String getStoreEmail() {
+        return storeEmail;
+    }
+    public String getStorePhone() {
+        return storePhone;
+    }
+    public String getStoreAddress() {
+        return storeAddress; 
+    }
+    public String getPizzaIngredients() {
+        return pizzaIngredients;
+    }
+    public double getPizzaPrice() {
+        return pizzaPrice;
+    }
+    public String getSides() {
+        return sides;
+    }
+    public String getDrinks() {
+        return drinks;
+    }
+    public String getOrderID() {
+        return orderID;
+    }
+    public double getOrderTotal() {
+        return orderTotal;
+    }
+    public void setStoremenu(String storemenu) {
+        this.storemenu = storemenu;
+    }
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+    public void setStoreEmail(String storeEmail) {
+        this.storeEmail = storeEmail;
+    }
+    public void setStorePhone(String storePhone) {
+        this.storePhone = storePhone;
+    }
+    public void setStoreAddress(String storeAddress) {
+        this.storeAddress = storeAddress;
+    }
+    public void setPizzaIngredients(String pizzaIngredients) {
+        this.pizzaIngredients = pizzaIngredients;
+    }
+    public void setPizzaPrice(double pizzaPrice) {
+        this.pizzaPrice = pizzaPrice;
+    }
+    public void setSides(String sides) {
+        this.sides = sides;
+    }
+    public void setDrinks(String drinks) {
+        this.drinks = drinks;
+    }
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+    public void setOrderTotal(double orderTotal) {
+        this.orderTotal = orderTotal;
+    }
+  
+
     public void takeOrder(String id,double total) {
      orderID = id;
      orderTotal = total;
@@ -41,7 +130,7 @@ public class Slice_o_Heaven {
          }
  
          public static void main(String[]args) {
-             Slice_o_Heaven pizzeria = new Slice_o_Heaven();
+             Slice_o_Heaven pizzeria = new Slice_o_Heaven("12345", "Some Ingredients", 50.00);
              pizzeria.takeOrder("12345", 50.00);
          }
         }
